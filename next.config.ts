f00f2199e97ede04f06a2439117ea03b342dffa7
Death-Vader <kumarprashant.0304@gmail.com>
2026-01-19 21:48:00 +0530

    Fix next config for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",      // <--- Enables static export for GitHub Pages
+  images: {
+    unoptimized: true,   // <--- Prevents image errors on GitHub Pages
+  },
 };
 
 export default nextConfig;
